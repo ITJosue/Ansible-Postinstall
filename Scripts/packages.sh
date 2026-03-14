af51a066acdf15f21b752c087276491421e45d78
Josue <118592169+ITJosue@users.noreply.github.com>
2026-03-14 05:36:07 -0500

    fixed syntax error

--- Scripts/packages.sh
+++ Scripts/packages.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LOG_DIR="${SCRIPT_DIR}/../logs"
+mkdir -p "${LOG_DIR}"
+LOG_FILE="${LOG_DIR}/packages_$(date +%Y%m%d_%H%M%S).log"
+
+exec > >(tee -a "${LOG_FILE}") 2>&1
+
+echo "Starting package installation. Logging to ${LOG_FILE}..."
+
 dnf install -y \
     @virtualization \
     7zip android-tools bleachbit calibre conda corectrl displaycal distrobox fastfetch fish gtk3-devel libva-utils pidgin pidgin-otr snapd tldr zsh \
@@ -14,6 +23,6 @@ dnf install -y \
     fail2ban fail2ban-firewalld fail2ban-systemd \
     zoom \
     google-noto-fonts-all google-noto-color-emoji-fonts rsms-inter-fonts \
-    boundary packer terraform vagrant vagrant-libvirt vagrant-cachier vagrant-sshfs vagrant-hostmanager vagrant-registration waypoint #hashicorp \
-    ffmpegthumbnailer #dolphin thumbnails
+    boundary packer terraform vagrant vagrant-libvirt vagrant-cachier vagrant-sshfs vagrant-hostmanager vagrant-registration waypoint \
+    ffmpegthumbnailer
 exit 0
